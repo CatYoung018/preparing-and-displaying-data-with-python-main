6b08fc847aa7f17cd6b7f1cce7b7a42ca73669e6
cat_young <cyoun002@gmail.com>
2025-11-17 08:51:54 -0500

    feat: Update chart with bigger font

--- add-style-to-your-pie-chart/main.py
+++ add-style-to-your-pie-chart/main.py
@@ -1,11 +1,13 @@
 import matplotlib.pyplot as plt
 
-snack_scores = [80,45,34]
+snack_scores = [100,80,60]
 
-slice_labels = ["Chocolate", "Cheese", "Pickles"]
+slice_labels = ["Chips", "Spicy Peanuts", "Reese's Cups"]
 
-plt.pie(snack_scores, labels=slice_labels)
+colors = ["#FAFAD2","#B22222","#FFA07A"]
 
-plt.title("Snack Scores")
+plt.pie(snack_scores, labels=slice_labels, colors=colors)
+
+plt.title("Snack Scores", fontsize=22)
 
 plt.savefig("snack_scores.png")

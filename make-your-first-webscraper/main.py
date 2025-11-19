c063bd004e735a491a7db824fae4176555c719cb
cat_young <cyoun002@gmail.com>
2025-11-19 14:57:35 -0500

    feat: Add BeautifulSoup to perform webscraping

--- make-your-first-webscraper/main.py
+++ make-your-first-webscraper/main.py
@@ -1,7 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
 
-honey_badger = requests.get("https://en.wikipedia.org/wiki/Honey_badger")
+# We define a 'User-Agent' so Wikipedia thinks we are a browser, not a bot.
+headers = {
+  'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+}
+
+# We add 'headers=headers' to the request
+honey_badger = requests.get("https://en.wikipedia.org/wiki/Honey_badger", headers=headers)
+# --- END OF FIX ---
 honey_badger.raise_for_status()
 honey_badger_html = honey_badger.text.encode("utf-8")
 honey_badger_soup = BeautifulSoup(honey_badger_html, 'html.parser')
+
+h2 = honey_badger_soup.find_all("h2")
+
+print(len(h2))
+
+for element in h2:
+  print(element)
+
+links = honey_badger_soup.find_all("a")
+print(links[:5])
+
+images = honey_badger_soup.find_all("img")
+print(images[:-4])

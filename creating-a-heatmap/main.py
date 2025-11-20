ad8a98347c92d46e180218318684ca9b7b5aa8cd
cat_young <cyoun002@gmail.com>
2025-11-20 08:21:49 -0500

    feat: Add heatmap to portfolio

--- creating-a-heatmap/main.py
+++ creating-a-heatmap/main.py
@@ -2,3 +2,17 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
+with open("tips.csv", "r") as csvfile:
+    tips = pd.read_csv(csvfile, delimiter=",")
+
+tips_pivoted = tips.pivot_table(values="tip", index=["size"], columns=["time"])
+
+fig = sns.heatmap(tips_pivoted, annot=True, cmap="coolwarm")
+
+fig.set_ylim(0,5)
+
+plt.xlabel("Time")
+plt.ylabel("Size")
+plt.title("Avg Tip Amount($) According to Time & Party Size")
+plt.savefig("tips_heatmap.png")
+
